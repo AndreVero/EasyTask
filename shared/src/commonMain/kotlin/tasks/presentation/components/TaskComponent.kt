da4ffa2c57817e6ffa374b7d21ce2570d1bc8c20
AndreVero <andriyveremiyenko@gmail.com>
2024-01-13 17:39:06 +0000

    Add drag animation to TaskComponent

--- shared/src/commonMain/kotlin/tasks/presentation/components/TaskComponent.kt
+++ shared/src/commonMain/kotlin/tasks/presentation/components/TaskComponent.kt
@@ -1,14 +1,17 @@
 package tasks.presentation.components
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -19,35 +22,77 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import io.github.aakira.napier.Napier
+import kotlinx.coroutines.launch
 import tasks.domain.model.Task
 
 @Composable
 fun TaskComponent(
     task: Task,
+    markTaskAsDone: () -> Unit,
+    markTaskAsFailed: () -> Unit,
+    distanceForSwipe: Dp = 150.dp,
     modifier: Modifier = Modifier
 ) {
 
     var expanded by remember { mutableStateOf(false) }
+    var offset by remember { mutableStateOf(DpOffset.Zero) }
+    val coroutineScope = rememberCoroutineScope()
+    val animatable by remember { mutableStateOf(Animatable(1f)) }
 
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .border(1.dp, Color.White, RoundedCornerShape(
-                topStartPercent = 10,
-                topEndPercent = 40,
-                bottomStartPercent = 10,
-                bottomEndPercent = 10)
+            .offset(x = offset.x * animatable.value, y = offset.y * animatable.value)
+            .border(
+                1.dp, Color.White, RoundedCornerShape(
+                    topStartPercent = 10,
+                    topEndPercent = 40,
+                    bottomStartPercent = 10,
+                    bottomEndPercent = 10
+                )
             )
             .padding(16.dp)
+            .pointerInput(true) {
+                detectDragGestures(
+                    onDragEnd = {
+                        if (offset.x > distanceForSwipe) {
+                            markTaskAsDone()
+                        } else if (offset.x < -distanceForSwipe) {
+                            markTaskAsFailed()
+                        } else {
+                            coroutineScope.launch {
+                                animatable.animateTo(0f)
+                                offset = DpOffset.Zero
+                                animatable.snapTo(1f)
+                            }
+                        }
+                    },
+                    onDrag = { change, dragAmount ->
+                        val x = offset.x.toPx()
+                        val y = offset.y.toPx()
+
+                        offset = DpOffset(
+                            x = (x + dragAmount.x).toDp(),
+                            y = y.toDp()
+                        )
+                    }
+                )
+            }
     ) {
-        Column(modifier = Modifier.align(Alignment.TopStart)){
+        Column(modifier = Modifier.align(Alignment.TopStart)) {
             Text(
                 text = "Title: " + task.title,
                 color = Color.White,
@@ -60,7 +105,7 @@ fun TaskComponent(
                 style = MaterialTheme.typography.body2,
             )
             Spacer(modifier = Modifier.height(8.dp))
-            AnimatedContent(expanded) {isExpanded ->
+            AnimatedContent(expanded) { isExpanded ->
                 if (isExpanded) {
                     Column {
                         Text(
