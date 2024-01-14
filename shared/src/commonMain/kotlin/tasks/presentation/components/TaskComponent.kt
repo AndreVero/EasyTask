b1850a1d5f233c1f7b111b2d30ce6a7a4271fc83
AndreVero <andriyveremiyenko@gmail.com>
2024-01-14 12:55:00 +0000

    Add JsonLottieAnimation

--- shared/src/commonMain/kotlin/tasks/presentation/components/TaskComponent.kt
+++ shared/src/commonMain/kotlin/tasks/presentation/components/TaskComponent.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalResourceApi::class)
+
 package tasks.presentation.components
 
 import androidx.compose.animation.AnimatedContent
@@ -9,6 +11,8 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -26,7 +30,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
@@ -34,8 +37,9 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import io.github.aakira.napier.Napier
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.resource
 import tasks.domain.model.Task
 
 @Composable
@@ -51,10 +55,13 @@ fun TaskComponent(
     var offset by remember { mutableStateOf(DpOffset.Zero) }
     val coroutineScope = rememberCoroutineScope()
     val animatable by remember { mutableStateOf(Animatable(1f)) }
+    var doneAnimationVisible by remember { mutableStateOf(false) }
+    var failedAnimationVisible by remember { mutableStateOf(false) }
 
     Box(
         modifier = modifier
             .fillMaxWidth()
+            .height(500.dp)
             .offset(x = offset.x * animatable.value, y = offset.y * animatable.value)
             .border(
                 1.dp, Color.White, RoundedCornerShape(
@@ -69,8 +76,10 @@ fun TaskComponent(
                 detectDragGestures(
                     onDragEnd = {
                         if (offset.x > distanceForSwipe) {
+                            doneAnimationVisible = true
                             markTaskAsDone()
                         } else if (offset.x < -distanceForSwipe) {
+                            failedAnimationVisible = true
                             markTaskAsFailed()
                         } else {
                             coroutineScope.launch {
@@ -92,6 +101,20 @@ fun TaskComponent(
                 )
             }
     ) {
+        if (doneAnimationVisible) {
+            JsonAnimation(
+                modifier = Modifier.fillMaxSize(),
+                fileName = resource("raw/anim2.json"),
+                iterations = 10
+            )
+        }
+        if (failedAnimationVisible) {
+            JsonAnimation(
+                modifier = Modifier.fillMaxSize(),
+                fileName = resource("raw/anim2.json"),
+                iterations = 10
+            )
+        }
         Column(modifier = Modifier.align(Alignment.TopStart)) {
             Text(
                 text = "Title: " + task.title,
